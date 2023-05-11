8d1f7eac56eb1ae24898d03aa70de1ae1e0193c9
yungshun317 <seraph317@gmail.com>
2023-05-11 08:12:04 +0800

    [FEAT] Create the main PHP file including header comment for telling WordPress that the file is a plugin and providing information about it

---
+++ slider.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Plugin Name: Slider
+ * Plugin URI: https://www.wordpress.org/slider
+ * Description: My plugin's description
+ * Version: 1.0
+ * Requires at least: 5.6
+ * Author: Yung-Shun Chang
+ * Author URI: https://www.chouqin.com.tw
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: slider
+ * Domain Path: /languages
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit;
+}
