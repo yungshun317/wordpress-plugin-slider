940801f109386cf88637c35396aaf1a8a73c2809
yungshun317 <seraph317@gmail.com>
2023-05-12 10:33:55 +0800

    [FEAT] Initialize the default metadata

--- post-types/class.slider-cpt.php
+++ post-types/class.slider-cpt.php
@@ -58,8 +58,18 @@ if ( ! class_exists( 'Slider_Post_Type' ) ) {
                 $old_link_url = get_post_meta( $post_id, 'slider_link_url', true );
                 $new_link_url = $_POST['slider_link_url'];
 
-                update_post_meta( $post_id, 'slider_link_text', $new_link_text, $old_link_text );
-                update_post_meta( $post_id, 'slider_link_url', $new_link_url, $old_link_url );
+                if ( empty( $new_link_text ) ) {
+                    update_post_meta( $post_id, 'slider_link_text', 'Add some text' );
+                } else {
+                    update_post_meta( $post_id, 'slider_link_text', sanitize_text_field( $new_link_text ), $old_link_text );
+                }
+
+                if ( empty( $new_link_url ) ) {
+                    update_post_meta( $post_id, 'slider_link_url', '#');
+                } else {
+                    update_post_meta( $post_id, 'slider_link_url', sanitize_text_field( $new_link_url ), $old_link_url );
+                }
+
             }
         }
 
