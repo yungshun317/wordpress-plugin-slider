23499b48178c0b9bd855390639adba0e0de37deb
yungshun317 <seraph317@gmail.com>
2023-05-16 20:40:42 +0800

    [FEAT] Validate fields

--- class.slider-settings.php
+++ class.slider-settings.php
@@ -12,7 +12,7 @@ if ( ! class_exists( 'Slider_Settings' ) ) {
 
         public function admin_init() {
 
-            register_setting( 'slider_group', 'slider_options' );
+            register_setting( 'slider_group', 'slider_options', array( $this, 'slider_validate' ) );
 
             add_settings_section(
                 'slider_main_section',
@@ -126,6 +126,24 @@ if ( ! class_exists( 'Slider_Settings' ) ) {
                 <?php endforeach; ?>
             </select>
             <?php
+        }
+
+        public function slider_validate( $input ) {
+            $new_input = array();
+            foreach( $input as $key => $value ) {
+                switch ( $key ) {
+                    case 'slider_title':
+                        if ( empty( $value ) ) {
+                            $value = 'Please, type some text';
+                        }
+                        $new_input[$key] = sanitize_text_field( $value );
+                    break;
+                    default:
+                        $new_input[$key] = sanitize_text_field( $value );
+                    break;
+                }
+            }
+            return $new_input;
         }
     }
 }
